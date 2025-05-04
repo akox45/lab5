597ca7eda2cec7dcbfef6ee1eb0fc8f4bff567ab
Your Name <you@example.com>
2025-05-04 08:25:08 -0400

    db version changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "random_id" "suffix" {
 resource "aws_db_instance" "postgres" {
   allocated_storage    = 20
   engine               = "postgres"
-  engine_version       = "15.4"
+  engine_version       = "15.5"
   instance_class       = "db.t3.micro"
   db_name              = "photoalbum"
   username             = "photoalbum"
