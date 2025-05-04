cd4e2e3a650a12f39180484b77cb914091ec3acf
Your Name <you@example.com>
2025-05-04 13:09:16 -0400

    Add RDS security group to allow connections from ECS tasks

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,7 @@ resource "aws_db_instance" "postgres" {
   password             = random_password.db_password.result
   skip_final_snapshot  = true
   publicly_accessible  = true
+  vpc_security_group_ids = [aws_security_group.rds.id]
 }
 
 resource "random_password" "db_password" {
@@ -81,6 +82,25 @@ resource "aws_security_group" "ecs" {
     to_port         = 8000
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "photoalbum-rds-sg"
+  description = "Allow PostgreSQL inbound traffic from ECS tasks"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs.id]
   }
   egress {
     from_port   = 0
