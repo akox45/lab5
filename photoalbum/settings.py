3bd3b0d50a357dce2070a4b1552f9aac2db852cf
Your Name <you@example.com>
2025-05-12 19:37:42 -0400

    fix: simplify S3 bucket configuration

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -13,21 +13,25 @@ DATABASES = {
     )
 }
 
-# AWS S3 konfiguráció
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-central-1')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+# S3 Storage settings
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-central-1')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Media files
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Statikus és media fájlok konfigurációja
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Debug mód kikapcsolása éles környezetben
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
--- terraform/main.tf
+++ terraform/main.tf
@@ -270,11 +270,28 @@ resource "aws_ecs_service" "django" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "photos" {
+  bucket = aws_s3_bucket.photos.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "photos" {
   bucket = aws_s3_bucket.photos.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "photos" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.photos,
+    aws_s3_bucket_public_access_block.photos,
+  ]
+
+  bucket = aws_s3_bucket.photos.id
+  acl    = "public-read"
 } 
